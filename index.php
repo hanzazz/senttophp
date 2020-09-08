e77b9e5d5ff95c88a0b69dcb0757a0e48097d681
hanzazz <we__j3ack@hotmail.co.th>
2020-09-08 13:08:51 +0700

    index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+$rawdata = file_get_contents('php://input');
+//$rawdata = "U41f193fcda59130f83cc2cc16e4929e9,60038527,0,1598434475828";
+
+$servername = "sql12.freemysqlhosting.net";
+$username = "sql12363661";
+$password = "2bkjcJHPBS";
+$dbname = "sql12363661";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) 
+{
+    die("Connection failed: " . $conn->connect_error);
+} 
+//echo "Connected successfully";
+$posted_data = explode(",", trim($rawdata));
+$UserID = $posted_data[0];
+$Account = $posted_data[1];
+$Text = $posted_data[2];
+
+
+//$sql1 = "INSERT INTO log (UserID, Text, Timestamp) VALUES ('$UserID', '$Text', '$Timestamp')";
+$sql1 = "UPDATE log SET Text='$Text' WHERE UserID='$UserID' AND Account='$Account'";
+if ($conn->query($sql1) === TRUE) {
+	echo "Record updated successfully";
+  }
+
+
+?>
